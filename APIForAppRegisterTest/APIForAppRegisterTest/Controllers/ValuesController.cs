fc9de4d6f1b5546114896a315e10b98aad4932c3
Rossall Sandford <rossall.sandford@ukho.gov.uk>
2019-06-28 10:57:52 +0100

    Add routing to Get

--- APIForAppRegisterTest/APIForAppRegisterTest/Controllers/ValuesController.cs
+++ APIForAppRegisterTest/APIForAppRegisterTest/Controllers/ValuesController.cs
@@ -8,6 +8,7 @@ namespace APIForAppRegisterTest.Controllers
     public class ValuesController : ControllerBase
     {
         // GET api/values
+        [Route("/")]
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
